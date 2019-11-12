dc8c3aa2453a720d85947634e923ff599f5297ee
Sharon Moriel <shmoriel@gmail.com>
2019-11-12 16:45:24 +0000

    create task manager

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'tasks#index'
+  # Read all tasks
+  get 'tasks', to: 'tasks#index'
+  # Read the details of a single task
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
+  # Create a new task (2 steps: get the form + post the params)
+  get 'new', to: 'tasks#new', as: :new_task
+  post 'tasks', to: 'tasks#create'
+
+  # Update a task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
+
+  # Delete a task
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
+
+# resources :tasks
